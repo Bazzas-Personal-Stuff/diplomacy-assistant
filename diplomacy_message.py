c887550e244d6d4ad585b33f9522b27edc8546d2
Mungo_ <bailey.gibbons2018@gmail.com>
2021-07-29 17:28:36 +1000

    Handle errors from the web scraper to prevent crashes

--- diplomacy_message.py
+++ diplomacy_message.py
@@ -1,6 +1,7 @@
 import os
 import dotenv
 import scraper
+import sys
 
 dotenv.load_dotenv()
 WD_GAME_ID = os.getenv('WD_GAME_ID')
@@ -26,42 +27,47 @@ def get_status() -> (str, str, bool, int, str):
         prev_phase = save_file.readline().strip()
 
     # check if date/phase are different
-    this_tuple = scraper.get_date_and_phase()
-    this_season = this_tuple[0].strip()
-    this_year = this_tuple[1].strip()
-    this_phase = this_tuple[2].strip()
-    if this_season != prev_season or this_year != prev_year or this_phase != prev_phase:
-        # send message with info
-        # diplomatic phase:     U+1F52B     \N{pistol}
-        # retreat phase:        U+1F4A2     \N{anger symbol}
-        # build phase:          U+1F528     \N{HAMMER}
+    try:
+        this_tuple = scraper.get_date_and_phase()
+        this_season = this_tuple[0].strip()
+        this_year = this_tuple[1].strip()
+        this_phase = this_tuple[2].strip()
+        if this_season != prev_season or this_year != prev_year or this_phase != prev_phase:
+            # send message with info
+            # diplomatic phase:     U+1F52B     \N{pistol}
+            # retreat phase:        U+1F4A2     \N{anger symbol}
+            # build phase:          U+1F528     \N{HAMMER}
 
-        phases = {
-            "Diplomacy": "\N{pistol} DIPLOMACY \N{pistol}\n",
-            "Retreats": "\N{anger symbol} RETREAT \N{anger symbol}\n",
-            "Builds": "\N{hammer} BUILD \N{hammer}\n",
-            "Pre-game": "\N{alarm clock} PRE-GAME \N{alarm clock}\n"
-        }
+            phases = {
+                "Diplomacy": "\N{pistol} DIPLOMACY \N{pistol}\n",
+                "Retreats": "\N{anger symbol} RETREAT \N{anger symbol}\n",
+                "Builds": "\N{hammer} BUILD \N{hammer}\n",
+                "Pre-game": "\N{alarm clock} PRE-GAME \N{alarm clock}\n"
+            }
 
-        colors = {
-            "Diplomacy": 0x47b484,
-            "Retreats": 0xf2454b,
-            "Builds": 0xf7a432,
-            "Pre-game": 0x7289da
-        }
+            colors = {
+                "Diplomacy": 0x47b484,
+                "Retreats": 0xf2454b,
+                "Builds": 0xf7a432,
+                "Pre-game": 0x7289da
+            }
 
-        message_string = phases[this_phase]
-        color = colors[this_phase]
-        comms = True if this_phase == "Diplomacy" or "Pre-game" else False
-        date = "{}, {}".format(this_season, this_year)
+            message_string = phases[this_phase]
+            color = colors[this_phase]
+            comms = True if this_phase == "Diplomacy" or "Pre-game" else False
+            date = "{}, {}".format(this_season, this_year)
 
-        url = scraper.get_map_url()
-        print("Sending message: {} {} {}".format(message_string, date, url))
+            url = scraper.get_map_url()
+            print("Sending message: {} {} {}".format(message_string, date, url))
 
-        with open(save_name, "wt") as save_file:
-            save_file.write("{}\n{}\n{}\n".format(this_season, this_year, this_phase))
+            with open(save_name, "wt") as save_file:
+                save_file.write("{}\n{}\n{}\n".format(this_season, this_year, this_phase))
 
-        return message_string, url, comms, color, date
-    else:
-        print("No progress detected.")
+            return message_string, url, comms, color, date
+        else:
+            print("No progress detected.")
+            return None
+
+    except Exception as e:
+        print(e, file=sys.stderr)
         return None
